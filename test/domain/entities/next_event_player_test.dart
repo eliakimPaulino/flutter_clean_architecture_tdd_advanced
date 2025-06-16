d102195e15a43ed5fc5ccb09db70b6733cf83cc9
eliakim <eliakim.paulino@gmail.com>
2025-06-16 11:40:31 -0300

    test: add NextEventPlayer class and corresponding test for initials extraction

---
+++ test/domain/entities/next_event_player_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  setUp(() {});
+
+  test('Should return the first letter of the first and last name', () {
+    final player = NextEventPlayer(
+      id: '',
+      name: 'Eliakim França',
+      isConfirmed: true,
+    );
+    expect(player.getInitials(), 'EF');
+  });
+}
+
+class NextEventPlayer {
+  final String id;
+  final String name;
+  final String? photo;
+  final String? position;
+  final bool isConfirmed;
+  final DateTime? confirmationDate;
+
+  NextEventPlayer({
+    required this.id,
+    required this.name,
+    this.photo,
+    this.position,
+    required this.isConfirmed,
+    this.confirmationDate,
+  });
+
+  String getInitials() {
+    final names = name.split(' ');
+    if (names.length < 2) return names.first.isNotEmpty ? names.first[0] : '';
+    return '${names.first[0]}${names.last[0]}';
+  }
+}
