17e23b174f69cc28cec2dd2a08da3fa97435ac95
eliakim <eliakim.paulino@gmail.com>
2025-06-18 11:18:27 -0300

    change nextEventLoader to use an astract repository class.

--- test/usecases/next_event_loader_test.dart
+++ test/usecases/next_event_loader_test.dart
@@ -5,24 +5,32 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   test('Should load event data from a repository.', () async {
     final groupId = Random().nextInt(50000).toString();
-    final repo = LoadNextEventRepository();
+    final repo = LoadNextEventMockRepository();
     final sut = NextEventLoader(repo: repo);
     await sut(groupId: groupId);
     expect(repo.groupId, groupId);
   });
 }
 
-class LoadNextEventRepository {
+class LoadNextEventMockRepository implements LoadNextEventRepository {
+  // classe mock da camada de infra.
   String? groupId;
   var callsCount = 0;
 
+  @override
   Future<void> loadNextEvent({required String groupId}) async {
     this.groupId = groupId;
     callsCount++;
   }
 }
 
+abstract class LoadNextEventRepository {
+  // classe da camada de infra.
+  Future<void> loadNextEvent({required String groupId});
+}
+
 class NextEventLoader {
+  // classe da camada de domínio.
   final LoadNextEventRepository repo;
 
   NextEventLoader({required this.repo});
